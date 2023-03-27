3482e8b885bcd08cc620658f71c6f9f960e93a40
Ramya S <82898795+RamyaSenapathy@users.noreply.github.com>
2023-03-27 13:40:01 -0400

    Update 2filter.py

--- 2filter.py
+++ 2filter.py
@@ -1,4 +1,5 @@
 
+
 import streamlit as st
 import altair as alt
 import numpy as np
@@ -20,12 +21,13 @@ click = alt.selection_multi(encodings=['color'])
 #scatter-plot
 scatter = alt.Chart(df).mark_point().encode(
     y='Happiness_score', tooltip=['average(Happiness_score)'],
+    #opacity=alt.value(0.2),
     color=alt.condition(select, 'Region', alt.value('lightgray'), legend=None)).add_selection(select).transform_filter(click)
 #Bar-plot
 bars = alt.Chart(df).mark_bar().encode(
     y='Region',color=alt.condition(click, 'Region', alt.value('lightgray')),
     x= alt.X('count(Region)',scale=alt.Scale(domain=[0, 500]))
-    ).add_selection(click).transform_filter(select).properties(width=150, height=250).properties(title='FILTER2 : Please select any region')
+    ).add_selection(click).transform_filter(select).properties(width=100, height=250).properties(title='FILTER2 : Please select any region')
 #scatter and Bar plot
 scatter.encode(alt.X(
     'Social_support',scale=alt.Scale(zero=False))).properties( height=250,width=230).properties(title='Happiness VS Social_support')|scatter.encode(alt.X(
@@ -40,7 +42,9 @@ st.subheader("Happiness by region")
 df2 = df[['Region','Happiness_score']]  
 bar2 = alt.Chart(df2, title='Tooltip: happiness score for each region').mark_bar(opacity=0.3,).encode(
     alt.X('Region',scale=alt.Scale(zero=False)),
-    alt.Y('mean(Happiness_score)',scale=alt.Scale(zero=False),title='Happiness score'), tooltip= 'mean(Happiness_score)', color= 'Region', opacity= "online:O").interactive()
+    alt.Y('mean(Happiness_score)',scale=alt.Scale(zero=False),title='Happiness score'), tooltip= 'mean(Happiness_score)',
+        color=alt.Color('Region'),
+      opacity= "online:O").interactive().properties(height=350,width=430)
 st.altair_chart(bar2, use_container_width=True)
 
 
